616859757c5533cacf484a4832b2db9a7a59a6f2
Mykhailo Kuznietsov <mkuznets@redhat.com>
2018-08-03 17:09:05 +0300

    fixup! Update license to EPL 2.0

--- plugins/plugin-serverservice/plugin-serverservice-ide/src/main/java/org/eclipse/che/sample/ide/ServerServiceExtension.java
+++ plugins/plugin-serverservice/plugin-serverservice-ide/src/main/java/org/eclipse/che/sample/ide/ServerServiceExtension.java
@@ -5,9 +5,6 @@
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *   Red Hat, Inc. - initial API and implementation
  */
 package org.eclipse.che.sample.ide;
 
